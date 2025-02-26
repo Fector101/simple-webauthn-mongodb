8f9185e513cb0d532769e4fb69086b986a15fcb1
fector101 <fabianjoseph063@gmail.com>
2025-02-26 02:34:50 +0100

    logic frontend

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,8 @@
+const { startRegistration, start} = SimpleWebAuthnBrowser;
 function displayHint(hint) {
     document.querySelector('#text').innerText = hint
 }
-const { startRegistration } = SimpleWebAuthnBrowser;
+
 async function signUpfingerprint(e){
     e.preventDefault();
     matric_no = document.querySelector('#matric').value
@@ -66,4 +67,72 @@ document.querySelector('#text').innerText = err;
 }
 }
 
+
+async function loginInWithfingerprint(e){
+    e.preventDefault();
+    matric_no = document.querySelector('#matric').value
+    student_name = document.querySelector('#name').value
+    try{
+
+        // Get challenge from server, challenge is used to verify the response from the client
+        const response = await fetch("/api/authn/init-reg", 
+            {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ matric_no,student_name }),
+            credentials: "include"
+        })
+
+        // Parse JSON response
+        const initResponse = await response.json()
+
+        if(initResponse.error){
+            displayHint(JSON.stringify(initResponse))
+            return
+        }
+        else if(initResponse.exists){
+            displayHint(`${initResponse.student_name} already exists`)
+            return
+        }
+        else if(initResponse.msg === 'xxx'){
+            displayHint('This device is not supported authentication')
+            return
+        }
+
+        // Create passkey
+        const registationJSON =await startRegistration(initResponse)
+        
+        console.log(registationJSON,'registationJSON var')
+        
+        // Save and verify passkey with server
+        const verify_response = await fetch("/api/authn/verify-reg", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({registationJSON, matric_no,student_name}),
+            credentials: "include"
+        })
+        const verifyResponse = await verify_response.json();
+
+        if(verifyResponse.error){
+            displayHint(JSON.stringify(initResponse))
+            return
+        }
+        else{
+            displayHint('Student Registered successfully')
+            // displayHint('Student Registered successfully')
+            // redirect to login page frm server with matric_no
+        }
+        console.log(verifyResponse,'verification var')
+            
+        
+}
+catch(err){
+document.querySelector('#text').innerText = err;
+    
+}
+}
+
+document.querySelector('#signupBtn').addEventListener('click', loginInWithfingerprint);
+
 document.querySelector('#signupBtn').addEventListener('click', signUpfingerprint);
+
--- src/routes/students.js
+++ src/routes/students.js
@@ -8,6 +8,10 @@ router.get('/signup', (req, res) => {
     res.sendFile(path.join(__dirname, '../../public/pages/signup.html'));
 })
 
+router.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../public/pages/login.html'));
+})
+
 router.get('/dashboard', (req, res) => {
     res.sendFile(path.join(__dirname, '../../public/pages/dashboard.html'));
 })
