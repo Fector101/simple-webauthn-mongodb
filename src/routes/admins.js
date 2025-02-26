40f5fb5f1ffebab840762b0f62fe55b4c6106983
fector101 <fabianjoseph063@gmail.com>
2025-02-26 07:58:14 +0100

    authMiddleware test3

--- src/routes/admins.js
+++ src/routes/admins.js
@@ -4,6 +4,34 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require('../models/Student');
 
+
+const authMiddleware = async (req, res, next) => {
+  try {
+    // Check for token in headers
+    const token = req.header('x-auth-token');
+    if (!token) {
+      return res.status(401).json({ error: 'No token, authorization denied' });
+    }
+
+    // Verify token
+  //   const decoded = jwt.verify(token, process.env.ADMIN_PS || 'admin');
+    
+    // Add user from payload
+  //   console.log(await decoded.id ,'stain ')
+    req.user = 'admin'
+    if (!req.user) {
+      return res.status(401).json({ error: 'User not found' });
+    }
+    
+    next();
+  } catch (err) {
+    res.status(401).json({ error: 'Token is not valid' });
+  }
+};
+
+
+
+
 const router = express.Router();
 
 // router.get('/admin-dashboard', async (req, res) => {
@@ -14,7 +42,19 @@ const router = express.Router();
 //       res.status(500).json({ message: 'Server error' });
 //     }
 // })
-
+router.get('/admin-dashboard', authMiddleware, async (req, res) => {
+  try {
+      // req.user is set by the middleware
+      res.json({ 
+      message: 'Admin dashboard accessed successfully', 
+      user: req.user
+      });
+      
+  } catch (err) {
+      console.error(err);
+      res.status(500).json({ error: 'Server error' });
+  }
+  });
 
 router.get('/admin-login', (req, res) => {
     res.sendFile(path.join(__dirname, '../../public/pages/admin.html'));
--- src/routes/authns.js
+++ src/routes/authns.js
@@ -297,31 +297,6 @@ router.post('/verify-auth', async (req, res) => {
 
 
 
-const authMiddleware = async (req, res, next) => {
-    try {
-      // Check for token in headers
-      const token = req.header('x-auth-token');
-      if (!token) {
-        return res.status(401).json({ error: 'No token, authorization denied' });
-      }
-  
-      // Verify token
-    //   const decoded = jwt.verify(token, process.env.ADMIN_PS || 'admin');
-      
-      // Add user from payload
-    //   console.log(await decoded.id ,'stain ')
-      req.user = 'admin'
-      if (!req.user) {
-        return res.status(401).json({ error: 'User not found' });
-      }
-      
-      next();
-    } catch (err) {
-      res.status(401).json({ error: 'Token is not valid' });
-    }
-};
-
-
 router.post('/admin-login', async (req, res) => {
     try {
       const { password } = req.body;
@@ -351,19 +326,5 @@ router.post('/admin-login', async (req, res) => {
     }
 });
 
-// 3. Protected Route Example
-router.get('/admin-dashboard', authMiddleware, async (req, res) => {
-try {
-    // req.user is set by the middleware
-    res.json({ 
-    message: 'Welcome to admin dashboard', 
-    user: req.user
-    });
-    
-} catch (err) {
-    console.error(err);
-    res.status(500).json({ error: 'Server error' });
-}
-});
 
 module.exports = router
