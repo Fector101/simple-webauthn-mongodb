dd48f5edc6b98451a403ef5fa0440b03996ea2c0
fector101 <fabianjoseph063@gmail.com>
2025-03-05 19:07:47 +0100

    signup/login db test 3

--- src/models/Student.js
+++ src/models/Student.js
@@ -1,3 +1,5 @@
+const mongoose = require('mongoose');
+
 const studentSchema = new mongoose.Schema({
   id: { type: String, required: true },
   matric_no: { type: String, required: true, unique: true },
