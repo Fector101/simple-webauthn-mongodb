84c15460017162bbab9b052d9007cd920ece866e
fector101 <fabianjoseph063@gmail.com>
2025-03-05 19:10:20 +0100

    signup/login db test 4

--- src/db-utils.js
+++ src/db-utils.js
@@ -1,22 +1,23 @@
+const Student = require('./models/Student');
 
 async function getAllStudents() {
-    const users = await User.find();
+    const users = await Student.find();
     return users.map(user => ({ name: user.student_name, matric_no: user.matric_no }));
 }
 
 
 async function getUserByMatricNo(matric_no) {
-    return await User.findOne({ matric_no });
+    return await Student.findOne({ matric_no });
 }
 
 
 async function createUser(id, matric_no, student_name, aaguid, passKey) {
-    const user = new User({ id, matric_no, student_name, aaguid, passKey });
+    const user = new Student({ id, matric_no, student_name, aaguid, passKey });
     await user.save();
 }
 
 async function getUserById(id) {
-    return await User.findOne({ id });
+    return await Student.findOne({ id });
 }
 
 module.exports = {
