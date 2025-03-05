139e2677f70384560317c60e1e040dd89483b3ea
Fabian <fabianjoseph063@gmail.com>
2025-03-05 20:47:36 +0100

    Update Student.js

--- src/models/Student.js
+++ src/models/Student.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const studentSchema = new mongoose.Schema({
-  id: { type: Buffer, required: true },
+  id: { type: String, required: true },
   matric_no: { type: String, required: true, unique: true },
   student_name: { type: String, required: true },
   passKey: {
