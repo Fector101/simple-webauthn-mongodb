d52b32948241bcc04c6127f03c8d67f675c2c245
fector101 <fabianjoseph063@gmail.com>
2025-03-05 19:05:49 +0100

    signup/login db test 2

--- src/models/Student.js
+++ src/models/Student.js
@@ -1,15 +1,13 @@
-const mongoose = require('mongoose');
 const studentSchema = new mongoose.Schema({
-    id: { type: String, required: true },
-    matric_no: { type: String, required: true, unique: true },
-    student_name: { type: String, required: true },
-    passKey: {
-      publicKey: { type: String, required: true },
-      deviceType: { type: String, required: true },
-      backedUp: { type: Boolean, required: true },
-      transports: { type: Array, required: true }
-    }
-  });
-  
-  const User = mongoose.model('Student', studentSchema);
-  
+  id: { type: String, required: true },
+  matric_no: { type: String, required: true, unique: true },
+  student_name: { type: String, required: true },
+  passKey: {
+    publicKey: { type: String, required: true },
+    deviceType: { type: String, required: true },
+    backedUp: { type: Boolean, required: true },
+    transports: { type: Array, required: true }
+  }
+});
+
+module.exports = mongoose.model('Student', studentSchema);
